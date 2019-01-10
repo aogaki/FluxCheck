c2a01a534e22bac78d3923a810f5a0c48b3798b1
Soiciro Aogaki <sohichiroh.aogaki@eli-np.ro>
2019-01-10 14:58:01 +0200

    Simple working, comman line version is done

---
+++ include/TStdData.hpp
@@ -0,0 +1,49 @@
+#ifndef TStdData_hpp
+#define TStdData_hpp 1
+
+#include <CAENDigitizerType.h>
+
+// constexpr uint32_t kNSamples = 256;
+// constexpr uint32_t kNSamples = 1024 + 512;
+constexpr uint32_t kNSamples = 1024;
+
+class TStdData
+{
+ public:
+  TStdData()
+  {
+    ModNumber = 0;
+    ChNumber = 0;
+    TimeStamp = 0;
+    ADC = 0;
+    for (uint32_t i = 0; i < kNSamples; i++) Waveform[i] = 0;
+  };
+
+  ~TStdData(){};
+
+  uint8_t ModNumber;
+  uint8_t ChNumber;
+  uint64_t TimeStamp;
+  int16_t ADC;
+  uint16_t Waveform[kNSamples];
+};
+
+struct SampleData {
+  unsigned char ModNumber;
+  unsigned char ChNumber;
+  unsigned long TimeStamp;
+  short ADC;
+  unsigned short Waveform[kNSamples];
+};
+
+constexpr int ONE_HIT_SIZE = sizeof(unsigned char) + sizeof(unsigned char) +
+                             sizeof(unsigned long) + sizeof(short) +
+                             (sizeof(unsigned short) * kNSamples);
+
+constexpr int kMaxPacketSize = 2000000 - ONE_HIT_SIZE;
+
+// For data transfer buffer
+// constexpr int BUFFER_SIZE = 1024 * 1024 * 2 * 16;
+constexpr int BUFFER_SIZE = 1024 * 2 * 16;
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,192 @@
+#include <fcntl.h>
+#include <termios.h>
+#include <time.h>
+#include <iostream>
+#include <random>
+#include <string>
+
+#include <TApplication.h>
+#include <TCanvas.h>
+#include <TGraph.h>
+#include <TH1.h>
+
+#include "TWaveRecord.hpp"
+
+#include <bsoncxx/builder/stream/document.hpp>
+#include <bsoncxx/builder/stream/helpers.hpp>
+#include <bsoncxx/json.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
+#include <mongocxx/stdx.hpp>
+#include <mongocxx/uri.hpp>
+
+using bsoncxx::builder::stream::document;
+using bsoncxx::builder::stream::finalize;
+
+using bsoncxx::builder::basic::kvp;
+using bsoncxx::builder::basic::make_document;
+
+int kbhit(void)
+{
+  struct termios oldt, newt;
+  int ch;
+  int oldf;
+
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+  ch = getchar();
+
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+  if (ch != EOF) {
+    ungetc(ch, stdin);
+    return 1;
+  }
+
+  return 0;
+}
+
+void UploadData(double currentTime, double rate)
+{
+  mongocxx::client conn{mongocxx::uri{}};
+
+  auto collection = conn["node-angular"]["posts"];
+  bsoncxx::builder::stream::document buf{};
+
+  buf << "title"
+      << "count"
+      << "content" << currentTime << "imagePath" << rate;
+  collection.insert_one(buf.view());
+  buf.clear();
+}
+
+int main(int argc, char **argv)
+{
+  std::random_device seed_gen;
+  std::default_random_engine engine(seed_gen());
+  std::poisson_distribution<> dist(100.);
+
+  // We can make instance only onece
+  // Making it at main function is enough
+  mongocxx::instance inst{};
+
+  auto SWtrigger = false;
+  auto vTh = -0.5;
+  auto timeInterval = 60;
+
+  for (auto i = 1; i < argc; i++) {
+    if (std::string(argv[i]) == "-s") {
+      SWtrigger = true;
+    } else if (std::string(argv[i]) == "-t") {
+      vTh = atof(argv[++i]);
+    } else if (std::string(argv[i]) == "-i") {
+      timeInterval = atof(argv[++i]);
+    }
+  }
+
+  TApplication app("testApp", &argc, argv);
+
+  int link = 0;
+  auto digi = new TWaveRecord(CAEN_DGTZ_USB, link);
+  digi->SetThreshold(vTh);
+
+  digi->Initialize();
+
+  digi->StartAcquisition();
+
+  TH1D *hisCharge = new TH1D("hisCharge", "test", 20000, 0, 20000);
+  TGraph *grWave = new TGraph();
+  grWave->SetMaximum(20000);
+  grWave->SetMinimum(0);
+  for (uint iSample = 0; iSample < kNSamples; iSample++) {
+    grWave->SetPoint(iSample, iSample * 2, 8000.);  // one sample 2 ns
+  }
+
+  TCanvas *canvas = new TCanvas();
+  TCanvas *canvas2 = new TCanvas();
+  canvas->cd();
+  hisCharge->Draw();
+
+  auto lastTime = time(0);
+  auto hitCounter = 0;
+
+  while (true) {
+    if (SWtrigger) {
+      auto hit = dist(engine);
+      for (auto j = 0; j < hit; j++) digi->SendSWTrigger();
+    }
+    digi->ReadEvents();
+
+    const int nHit = digi->GetNEvents();
+    hitCounter += nHit;
+
+    auto dataArray = digi->GetDataArray();
+    for (int i = 0; i < nHit; i++) {
+      auto index = (i * ONE_HIT_SIZE);
+      auto offset = 0;
+      SampleData data;
+
+      data.ModNumber = dataArray[index + offset];
+      offset += sizeof(data.ModNumber);
+
+      data.ChNumber = dataArray[index + offset];
+      offset += sizeof(data.ChNumber);
+
+      memcpy(&data.TimeStamp, &dataArray[index + offset],
+             sizeof(data.TimeStamp));
+      offset += sizeof(data.TimeStamp);
+
+      memcpy(&data.ADC, &dataArray[index + offset], sizeof(data.ADC));
+      offset += sizeof(data.ADC);
+      if (data.ChNumber == 0) {
+        hisCharge->Fill(data.ADC);
+
+        for (uint iSample = 0; iSample < kNSamples; iSample++) {
+          unsigned short pulse;
+          memcpy(&pulse, &dataArray[index + offset], sizeof(pulse));
+          offset += sizeof(pulse);
+
+          grWave->SetPoint(iSample, iSample * 2, pulse);  // one sample 2 ns
+        }
+      }
+    }
+
+    auto currentTime = time(0);
+    if ((currentTime - lastTime) >= timeInterval) {
+      double hitRate = hitCounter / (currentTime - lastTime);
+      hitCounter = 0;
+      lastTime = currentTime;
+      std::cout << currentTime << "\t" << hitRate << std::endl;
+      UploadData(currentTime, hitRate);
+    }
+
+    canvas2->cd();
+    grWave->Draw("AL");
+    canvas2->Update();
+
+    canvas->cd();
+    hisCharge->Draw();
+    canvas->Update();
+
+    if (kbhit()) {
+      break;
+    } else {
+      usleep(1000);
+    }
+  }
+
+  digi->StopAcquisition();
+
+  // app.Run();
+  delete digi;
+
+  // All ROOT classes are deleted by ROOT.
+
+  return 0;
+}
