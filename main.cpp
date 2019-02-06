0a7e1a34e558a491b14604764cc39dc5f2b4933a
Soiciro Aogaki <sohichiroh.aogaki@eli-np.ro>
2019-02-06 12:26:49 +0200

    Not bad working version.  Including THttp and MongoDB access

--- main.cpp
+++ main.cpp
@@ -1,15 +1,34 @@
 #include <fcntl.h>
 #include <termios.h>
+#include <time.h>
+#include <fstream>
 #include <iostream>
 
 #include <TApplication.h>
 #include <TCanvas.h>
 #include <TGraph.h>
 #include <TH1.h>
+#include <THttpServer.h>
+#include <TStyle.h>
+#include <TSystem.h>
 
 // #include "TDPP.hpp"
 #include "TPSD.hpp"
 #include "TWaveRecord.hpp"
+
+#include <bsoncxx/builder/stream/document.hpp>
+#include <bsoncxx/builder/stream/helpers.hpp>
+#include <bsoncxx/json.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
+#include <mongocxx/stdx.hpp>
+#include <mongocxx/uri.hpp>
+
+using bsoncxx::builder::stream::document;
+using bsoncxx::builder::stream::finalize;
+
+using bsoncxx::builder::basic::kvp;
+using bsoncxx::builder::basic::make_document;
 
 int kbhit(void)
 {
@@ -37,13 +56,53 @@ int kbhit(void)
   return 0;
 }
 
+void UploadData(double currentTime, double rate)
+{
+  // mongocxx::client conn{mongocxx::uri{}};
+  mongocxx::client conn{mongocxx::uri{"mongodb://192.168.161.73/"}};
+
+  auto collection = conn["node-angular"]["posts"];
+  bsoncxx::builder::stream::document buf{};
+
+  buf << "title"
+      << "count"
+      << "content" << currentTime << "imagePath" << rate;
+  collection.insert_one(buf.view());
+  buf.clear();
+}
+
 int main(int argc, char **argv)
 {
+  // We can make instance only onece
+  // Making it at main function is enough
+  mongocxx::instance inst{};
+
+  auto timeInterval = 60;
+  auto linkNumber = 0;
+  for (auto i = 1; i < argc; i++) {
+    if (std::string(argv[i]) == "-i") {
+      timeInterval = atof(argv[++i]);
+    }
+    if (std::string(argv[i]) == "-l") {
+      linkNumber = atof(argv[++i]);
+    }
+  }
+
   TApplication app("testApp", &argc, argv);
 
-  int link = 0;
-  auto digi = new TPSD(CAEN_DGTZ_USB, link);
-  // auto digi = new TDPP(CAEN_DGTZ_USB, link);
+  std::ifstream fin("par.dat");
+  std::string key, val;
+  int startBin, stopBin;
+  while (fin >> key >> val) {
+    if (key == "Start")
+      startBin = std::stoi(val);
+    else if (key == "Stop")
+      stopBin = std::stoi(val);
+  }
+  fin.close();
+
+  auto digi = new TPSD(CAEN_DGTZ_USB, linkNumber);
+  // auto digi = new TDPP(CAEN_DGTZ_USB, linkNumber);
   digi->Initialize();
   digi->StartAcquisition();
 
@@ -56,16 +115,24 @@ int main(int argc, char **argv)
   canvas->cd();
   hisCharge->Draw();
 
-  for (int i = 0; true; i++) {
+  gStyle->SetOptFit(1111);
+  auto server = new THttpServer("http:8080?monitoring=5000;rw;noglobal");
+  server->Register("", hisCharge);
+
+  gSystem->ProcessEvents();
+
+  auto lastTime = time(0);
+  auto lastHit = 0;
+  for (int loopCounter = 0; true; loopCounter++) {
     //   // if (i > 10) break;
-    std::cout << i << std::endl;
+    // std::cout << i << std::endl;
 
     // for (int j = 0; j < 10; j++) digi->SendSWTrigger();
     digi->ReadEvents();
 
     auto dataArray = digi->GetDataArray();
     const int nHit = digi->GetNEvents();
-    std::cout << nHit << std::endl;
+    // std::cout << nHit << std::endl;
     for (int i = 0; i < nHit; i++) {
       auto index = (i * ONE_HIT_SIZE);
       auto offset = 0;
@@ -97,13 +164,29 @@ int main(int argc, char **argv)
       }
     }
 
-    canvas2->cd();
-    grWave->Draw("AL");
-    canvas2->Update();
+    auto currentTime = time(0);
+    if ((currentTime - lastTime) >= timeInterval) {
+      auto hitCounter = hisCharge->Integral(startBin, stopBin) - lastHit;
+      auto hitRate = hitCounter / (currentTime - lastTime);
+      std::cout << currentTime << "\t" << hitRate << " Hz\t" << hitCounter
+                << " hit" << std::endl;
+      // UploadData(currentTime, hitRate);
+      UploadData(currentTime, hitCounter);
+      lastHit += hitCounter;
+      lastTime = currentTime;
+    }
 
-    canvas->cd();
-    hisCharge->Draw();
-    canvas->Update();
+    if ((loopCounter % 1000) == 0) {
+      canvas2->cd();
+      grWave->Draw("AL");
+      canvas2->Update();
+
+      canvas->cd();
+      hisCharge->Draw();
+      canvas->Update();
+
+      gSystem->ProcessEvents();
+    }
 
     if (kbhit()) break;
 
@@ -114,6 +197,8 @@ int main(int argc, char **argv)
 
   // app.Run();
   delete digi;
+
+  delete server;
 
   return 0;
 }
